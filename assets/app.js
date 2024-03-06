16443ebf2b40f79738272d52d56ce9744dd5f340
isall <isall@teliae.fr>
2024-03-06 14:44:40 +0100

    feat: about, profil(add bootstrap offcanvas)

--- assets/app.js
+++ assets/app.js
@@ -6,3 +6,37 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+
+
+$(function(){
+
+    // détecte le scroll de la fenêtre et affiche ou non la photo de profil
+    var profilPicture = $('#profil-picture');
+    var navbarNameJob = $('#navbar-name-job');
+
+    $(window).on('scroll', (e) => {
+        var scroll = $(window).scrollTop();
+
+        if(scroll > 470)
+        {
+            // masque la photo de profil
+            profilPicture.addClass('hidden-element');
+            profilPicture.removeClass('show-element');
+            // affiche le texte avec le nom et prénom
+            navbarNameJob.css('display', 'block');
+            navbarNameJob.addClass('show-element animate__animated animate__bounce');
+            navbarNameJob.removeClass('hidden-element');
+        } else {
+            // affiche la photo de profil
+            profilPicture.addClass('show-element');
+            profilPicture.removeClass('hidden-element');
+            // masque le texte avec nom et prénom
+            navbarNameJob.css('display', 'none');
+            navbarNameJob.addClass('hidden-element');
+            navbarNameJob.removeClass('show-element animate__animated animate__bounce');
+        }
+        
+    });
+
+    
+});
