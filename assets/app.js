4c19f25e58aafc4097de6b19660f4e9db3998a40
isall <isall@teliae.fr>
2024-03-06 15:19:40 +0100

    feat: ajout de owl-carousel et de la bannière certification

--- assets/app.js
+++ assets/app.js
@@ -38,5 +38,29 @@ $(function(){
         
     });
 
-    
+    // owl-carousel js
+    $(".owl-carousel").owlCarousel({
+        //Basic Speeds
+        slideSpeed : 200,
+        paginationSpeed : 800,
+     
+        //Autoplay
+        autoPlay : true,
+        goToFirst : true,
+        goToFirstSpeed : 1000,
+     
+        // Navigation
+        navigation : false,
+        navigationText : ["prev","next"],
+        pagination : true,
+        paginationNumbers: false,
+     
+        // Responsive
+        responsive: true,
+        items : 5,
+        itemsDesktop : [1199,4],
+        itemsDesktopSmall : [980,3],
+        itemsTablet: [768,2],
+        itemsMobile : [479,1]
+    });
 });
